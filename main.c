93e0e990493555cfac492ec92121ab0daf5cd46b
NULL <aqeelprogrammer@gmail.com>
2026-06-23 00:31:52 +0300

    feat: add and use stat function from libc and use the errno and strerror

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <string.h>
 
 void print_error(const char *message) {
     printf("\x1b[31m"); // red
@@ -6,12 +9,18 @@ void print_error(const char *message) {
     printf("\x1b[0m");  // reset
 }
 
-int main(int argc, char *argv[]) {
-    if (argc > 1) {
-        printf("the first arg is %s\n", argv[1]);
-    }
+int main(/*int argc, char *argv[]*/) {
+    // if (argc > 1) {
+    //     printf("the first arg is %s\n", argv[1]);
+    // }
 
-    print_error("this is error!");
+    // print_error("this is error!");
+
+    struct stat info;
+
+    if (stat("./.gitignor", &info) == -1) {
+        print_error(strerror(errno));
+    }
 
     return 0;
 }
