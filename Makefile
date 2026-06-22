46171090f82c76e01482c1003a4a6cb6453437f4
NULL <aqeelprogrammer@gmail.com>
2026-06-22 23:06:20 +0300

    feat: add print_error function to print error with red color and test it and add make file to facilitate build process

---
+++ Makefile
@@ -0,0 +1,4 @@
+CFLAGS = -Wall -Wextra -Wpedantic -Werror
+
+file-info: main.c
+	cc $(CFLAGS) main.c -o file-info
--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
+void print_error(const char *message) {
+    printf("\x1b[31m"); // red
+    printf("Error: %s\n", message);
+    printf("\x1b[0m");  // reset
+}
+
 int main(int argc, char *argv[]) {
     if (argc > 1) {
         printf("the first arg is %s\n", argv[1]);
     }
+
+    print_error("this is error!");
 
     return 0;
 }
