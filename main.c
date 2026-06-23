b8666d3dde6861468484330f4b3fdbc8c28ca7c2
NULL <aqeelprogrammer@gmail.com>
2026-06-23 23:15:09 +0300

    feat: implement the minimal functionality of the tool

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #define _GNU_SOURCE
 
+#define INFO_COLOR  "\x1b[34m"
+#define ERROR_COLOR "\x1b[31m"
+#define RESET_COLOR "\x1b[0m"
+#define HORIZONTAL_LINE "―――――――――――――――――――――――――――――――――――――――――――\n"
+
 #include <stdio.h>
 #include <sys/stat.h>
 #include <errno.h>
@@ -7,15 +12,15 @@
 #include <stdlib.h>
 
 void print_error(const char *message) {
-    printf("\x1b[31m"); // red
+    printf(ERROR_COLOR); // red
     printf("Error: %s\n", message);
-    printf("\x1b[0m");  // reset
+    printf(RESET_COLOR);  // reset
 }
 
 struct FileInfo
 {
     const char *file_name;
-    unsigned long file_size;
+    size_t file_size;
 
 };
 
@@ -33,6 +38,14 @@ int get_file_info(const char *file_path, struct FileInfo *file_info) {
     return 0;
 }
 
+void print_file_info(const struct FileInfo *file_info) {
+    printf(INFO_COLOR); // red
+    printf(HORIZONTAL_LINE);
+    printf("File name: %s\n", file_info->file_name);
+    printf("File size(in byets): %zu\n", file_info->file_size);
+    printf(HORIZONTAL_LINE);
+    printf(RESET_COLOR);  // reset
+}
 
 int main(int argc, char *argv[]) {
     if (argc <= 1) {
@@ -42,14 +55,22 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    struct stat info;
+    size_t file_path_len = strlen(argv[1]);
+    char *file_path = malloc(file_path_len + 1);
+    strcpy(file_path, argv[1]);
 
-    if (stat(argv[1], &info) == -1) {
-        print_error(strerror(errno));
+    struct FileInfo file_info;
+
+    if (get_file_info(file_path, &file_info) < 0) {
+        print_error("cannot get file info");
         return EXIT_FAILURE;
     }
 
-    printf("the size of the file in bytes is %ld\n", info.st_size);
+    print_file_info(&file_info);
+
+    free(file_path);
+
+    file_path = NULL; 
 
     return EXIT_SUCCESS;
 }
