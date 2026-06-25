303fc11e8a1c3b7a2f4d376258aa17d0b396a7a7
NULL <aqeelprogrammer@gmail.com>
2026-06-25 16:12:44 +0300

    feat: make the file size more readable to the user

--- include/display.h
+++ include/display.h
@@ -5,5 +5,6 @@
 
 void print_error(const char *message);
 void print_file_info(const struct FileInfo *file_info);
+void full_size_string(size_t bytes, char *out_buffer, size_t buffer_size);
 
 #endif
--- src/display.c
+++ src/display.c
@@ -3,6 +3,10 @@
 #define RESET_COLOR "\x1b[0m"
 #define HORIZONTAL_LINE "―――――――――――――――――――――――――――――――――――――――――――\n"
 
+#define ONE_GB (1024 * 1024 * 1024)
+#define ONE_MB (1024 * 1024)
+#define ONE_KB (1024)
+
 #include "display.h"
 #include "file_info.h"
 #include <stdio.h>
@@ -14,10 +18,28 @@ void print_error(const char *message) {
 }
 
 void print_file_info(const struct FileInfo *file_info) {
+    char size_string[32];
+
     printf(INFO_COLOR); // red
     printf(HORIZONTAL_LINE);
+    
     printf("File name: %s\n", file_info->file_name);
-    printf("File size(in byets): %zu\n", file_info->file_size);
+
+    full_size_string(file_info->file_size, size_string, sizeof(size_string));
+    printf("File size: %s\n", size_string);
+    
     printf(HORIZONTAL_LINE);
     printf(RESET_COLOR);  // reset
+}
+
+void full_size_string(size_t bytes, char *out_buffer, size_t buffer_size) {
+    if (bytes >= ONE_GB) {
+        snprintf(out_buffer, buffer_size, "%.2f GB", (float)bytes / ONE_GB);
+    } else if (bytes >= ONE_MB) {
+        snprintf(out_buffer, buffer_size, "%.2f MB", (float)bytes / ONE_MB);
+    } else if (bytes >= ONE_KB) {
+        snprintf(out_buffer, buffer_size, "%.2f KB", (float)bytes / ONE_KB);
+    } else {
+        snprintf(out_buffer, buffer_size, "%zu bytes", bytes);
+    }
 }
